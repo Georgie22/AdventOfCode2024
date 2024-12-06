0c5c78d283a701bccd78a9b1a3d3af2e82a29ffa
Georgie22 <georgina@cooperteam.co.uk>
2024-12-06 14:42:13 +0000

    remove abspath in redad file lines

--- day01/src/day01puzzle/part1.py
+++ day01/src/day01puzzle/part1.py
@@ -4,8 +4,7 @@ from typing import List
 
 def read_file_lines(input_path: str) -> list[str]:
 
-    full_input_path = os.path.abspath(input_path)
-    with open(full_input_path, "r") as file:
+    with open(input_path, "r") as file:
         return file.readlines()
 
 
