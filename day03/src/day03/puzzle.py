5d0cce2b88bcd5b3848ce251844f1c0010322d35
Georgie22 <georgina@cooperteam.co.uk>
2024-12-10 10:32:01 +0000

    day3 part2

--- day03/src/day03/puzzle.py
+++ day03/src/day03/puzzle.py
@@ -17,15 +17,47 @@ def read_file_lines(input_path: str) -> list[str]:
 
     with open(input_path, "r") as file:
         return file.readlines()
-  
+    
 
-def find_instructions(input_path: str) -> list[str]:
+def get_corrupted_memory(input_path: str) -> str:
 
     corrupted_memory_lines = read_file_lines(input_path=input_path)
     corrupted_memory = "".join(corrupted_memory_lines)
-    instructions = re.findall(r"mul\(\d{1,3},\d{1,3}\)", corrupted_memory)
+
+    return corrupted_memory
+  
+
+def find_instructions(corrupted_memory: str) -> list[str]:
+
+    instructions =  re.findall(r"mul\(\d{1,3},\d{1,3}\)", corrupted_memory)
 
     return instructions
+
+
+def find_enabled_instructions(corrupted_memory: str) -> list[str]:
+
+    dont_splits = re.split(r"don't\(\)", corrupted_memory)
+
+    enabled_instructions = []
+
+    # the memory starts enabled as default
+    # so any instructions in the first split (before the first don't() condition) are automatically enabled
+    initial_instructions = find_instructions(dont_splits[0])
+    enabled_instructions.extend(initial_instructions)
+
+    # any instructions after don't() are ignored unless a do() condition is found. Instructions after do() are enabled
+    for split in dont_splits[1:]:
+        do_split = re.split(r"do\(\)", split)
+        if len(do_split) == 1:
+            # no do condition found
+            continue
+        else:
+            # accomodate for possibly more than one do() found
+            for split in do_split[1:]:
+                instructions = find_instructions(split)
+                enabled_instructions.extend(instructions)
+
+    return enabled_instructions
 
 
 def get_product_pairs(instructions: List[str]) -> list[list[int]]:
@@ -47,14 +79,29 @@ def calculate_sum_of_products(product_pairs: List[List[int]]) -> int:
     return sum_of_products
 
 
+def run_program(input_path: str, enable_conditions: bool = False) -> int:
+
+    corrupted_memory = get_corrupted_memory(input_path=input_path)
+    if enable_conditions:
+         instructions = find_enabled_instructions(corrupted_memory)
+    else:
+        instructions = find_instructions(corrupted_memory=corrupted_memory)
+    product_pairs = get_product_pairs(instructions=instructions)
+    sum_of_products = calculate_sum_of_products(product_pairs=product_pairs)
+
+    return sum_of_products
+
+
 def main(input_path: str) -> None:
 
     # part 1
-    instructions = find_instructions(input_path=input_path)
-    product_pairs = get_product_pairs(instructions=instructions)
-    sum_of_products = calculate_sum_of_products(product_pairs=product_pairs)
+    sum_of_products = run_program(input_path=input_path)
     print(sum_of_products)
+
+    # part 2
+    enabled_sum_of_products = run_program(input_path=input_path, enable_conditions=True)
+    print(enabled_sum_of_products)
 
 
 if __name__ == "__main__":
-    main("day03/inputs/input.txt")
+    main(input_path = "day03/inputs/input.txt")
--- day03/test/test_day03.py
+++ day03/test/test_day03.py
@@ -4,13 +4,13 @@ from day03.src.day03 import puzzle
 
 
 @pytest.fixture
-def input_path():
-    return "day03/test/test_inputs/test_input.txt"
+def corrupted_memory():
+    return "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 
 @pytest.fixture
 def instructions():
-    return ['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']
+    return ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"]
 
 
 @pytest.fixture
@@ -18,19 +18,39 @@ def product_pairs():
     return [[2, 4], [5, 5], [11, 8], [8, 5]]
 
 
-def test_find_instructions(input_path, instructions):
-    input_instructions = puzzle.find_instructions(input_path=input_path)
+@pytest.fixture
+def enabled_instructions():
+    return ["mul(2,4)", "mul(8,5)"]
+
+
+@pytest.fixture
+def enabled_product_pairs():
+    return [[2, 4], [8, 5]]
+
+
+def test_find_instructions(corrupted_memory, instructions):
+    input_instructions = puzzle.find_instructions(corrupted_memory=corrupted_memory)
 
     assert input_instructions == instructions
 
 
-def test_get_product_pairs(instructions, product_pairs):
+def test_find_enabled_instructions(corrupted_memory, enabled_instructions):
+    input_enabled_instructions = puzzle.find_enabled_instructions(corrupted_memory=corrupted_memory)
+
+    assert input_enabled_instructions == enabled_instructions
+
+
+def test_get_product_pairs(instructions, product_pairs, enabled_instructions, enabled_product_pairs):
     input_product_pairs = puzzle.get_product_pairs(instructions=instructions)
+    input_enabled_product_pairs = puzzle.get_product_pairs(instructions=enabled_instructions)
 
     assert input_product_pairs == product_pairs
+    assert input_enabled_product_pairs == enabled_product_pairs
 
 
-def test_calculate_sum_of_products(product_pairs):
+def test_calculate_sum_of_products(product_pairs, enabled_product_pairs):
     sum_of_product = puzzle.calculate_sum_of_products(product_pairs=product_pairs)
+    enabled_sum_of_product = puzzle.calculate_sum_of_products(product_pairs=enabled_product_pairs)
 
     assert sum_of_product == 161
+    assert enabled_sum_of_product == 48
