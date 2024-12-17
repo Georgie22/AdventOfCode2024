c5781a15629818bb1862b1d514b05c1b186388ab
Georgie22 <georgina@cooperteam.co.uk>
2024-12-17 14:16:27 +0000

    correct i, j and tidy function/variable names

--- day04/src/day04/puzzle.py
+++ day04/src/day04/puzzle.py
@@ -8,35 +8,43 @@ CROSS_DIRECTIONS = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
 
 @dataclass
 class Position():
-    i: int         # columns - corresponds to width
-    j: int         # rows - corresponds to height
+    i: int         # row
+    j: int         # column
 
 
 def read_file_lines(input_path: str) -> list[str]:
 
     with open(input_path, "r") as file:
-        return file.readlines()
+        lines = file.readlines()
+
+    return lines
     
 
-def create_grid(input_path: str) -> list[list]:
+def read_wordsearch(input_path: str) -> list[list[str]]:
 
-    grid = []
-    lines = read_file_lines(input_path=input_path)
-    for line in lines:
-        line_elements = list(line.split()[0])
-        grid.append(line_elements)
-    return grid
+    wordsearch = []
+    rows = read_file_lines(input_path=input_path)
+
+    for row in rows:
+        # remove new line element and take out of the list .split() creates
+        row_string = row.split()[0]
+
+        # separate the string into the individual characters to form the wordsearch row
+        row_elements = [elem for elem in row_string]
+        wordsearch.append(row_elements)
+
+    return wordsearch
     
 
-def check_position_in_grid(position: Type[Position], grid_size: tuple) -> bool:
+def check_position_in_wordsearch(position: Position, wordsearch_size: tuple) -> bool:
 
-    if (position.i >= 0 and position.i < grid_size[0] and position.j >= 0 and position.j < grid_size[1]):
+    if (position.i >= 0 and position.i < wordsearch_size[0] and position.j >= 0 and position.j < wordsearch_size[1]):
         return True
     else:
         return False
     
 
-def check_feasible_directions(position: Type[Position], max_index: int, grid_size: tuple, directions: List[tuple]) -> list[tuple]:
+def check_feasible_directions(position: Position, max_index: int, wordsearch_size: tuple, directions: List[tuple]) -> list[tuple]:
 
     feasible_directions = []
 
@@ -46,33 +54,34 @@ def check_feasible_directions(position: Type[Position], max_index: int, grid_siz
         j = position.j + d[1]*(max_index)
         end_position = Position(i, j)
 
-        in_bounds = check_position_in_grid(position=end_position, grid_size=grid_size)
+        in_bounds = check_position_in_wordsearch(position=end_position, wordsearch_size=wordsearch_size)
         if in_bounds:
             feasible_directions.append(d)
 
     return feasible_directions
 
 
-def check_for_pattern_match(grid: List[List], position: Type[Position], pattern: List, direction: tuple) -> bool:
+def check_for_pattern_match(wordsearch: List[List[str]], position: Position, pattern: List, direction: tuple) -> bool:
 
-    grid_pattern = []
+    wordsearch_pattern = []
     for n in range(len(pattern)):
 
         i = position.i + direction[0]*n
-        j = position.j + direction[1]*n
-        grid_element = grid[j][i] 
-        grid_pattern.append(grid_element)
+        j = position.j + direction[1]*n 
+        wordsearch_element = wordsearch[i][j]
+        wordsearch_pattern.append(wordsearch_element)
     
-    if grid_pattern == pattern:
+    if wordsearch_pattern == pattern:
         return True
     else:
         return False
     
 
-def check_for_cross_pattern_match(grid: List[List], position: Type[Position], pattern: List, directions: List[tuple]) -> bool:
+def check_for_cross_pattern_match(wordsearch: List[List[str]], position: Position, pattern: List, directions: List[tuple]) -> bool:
 
-    back_elements = {(0,0): grid[position.j][position.i]}
-    forward_elements = {(0,0): grid[position.j][position.i]}
+    back_cross_elements = {(0,0): wordsearch[position.i][position.j]}
+    forward_cross_elements = {(0,0): wordsearch[position.i][position.j]}
+
 
     # get grid elements and store in dictionary
     for d in directions:
@@ -83,15 +92,19 @@ def check_for_cross_pattern_match(grid: List[List], position: Type[Position], pa
 
             i = position.i + d_i
             j = position.j + d_j
-            grid_element = grid[j][i]
+            wordsearch_element = wordsearch[i][j]
 
+            # add the elements found in the (-1, -1) and (1, 1) directions to the back cross elements
             if (d_i < 0 and d_j < 0) or (d_i > 0 and d_j > 0):
-                back_elements[(d_i, d_j)] = grid_element
-            else:
-                forward_elements[(d_i, d_j)] = grid_element
+                back_cross_elements[(d_i, d_j)] = wordsearch_element
 
-    sorted_back_elements = [back_elements[k] for k in sorted(back_elements.keys())]
-    sorted_forward_elements = [forward_elements[k] for k in sorted(forward_elements.keys())]
+            # add the elements found in the (-1, 1) and (1, -1) directions to the forward cross elements
+            else:
+                forward_cross_elements[(d_i, d_j)] = wordsearch_element
+
+    # order the cross elements
+    sorted_back_elements = [back_cross_elements[k] for k in sorted(back_cross_elements.keys())]
+    sorted_forward_elements = [forward_cross_elements[k] for k in sorted(forward_cross_elements.keys())]
 
     if (sorted_back_elements == pattern or list(reversed(sorted_back_elements)) == pattern) and (sorted_forward_elements == pattern or list(reversed(sorted_forward_elements)) == pattern):
         return True
@@ -99,47 +112,47 @@ def check_for_cross_pattern_match(grid: List[List], position: Type[Position], pa
         return False
 
 
-def get_pattern_count_in_grid(input_path: str, pattern: str, directions: List[tuple]) -> int:
+def get_pattern_count_in_wordsearch(input_path: str, pattern: str, directions: List[tuple]) -> int:
 
-    grid = create_grid(input_path=input_path)
-    grid_height = len(grid)
-    grid_width = len(grid[0])
+    wordsearch = read_wordsearch(input_path=input_path)
+    wordsearch_height = len(wordsearch)
+    wordsearch_width = len(wordsearch[0])
 
     pattern = list(pattern)
   
     count = 0
-    for j in range(grid_height):
-        for i in range(grid_width):
+    for i in range(wordsearch_height):
+        for j in range(wordsearch_width):
 
-            if grid[j][i] == pattern[0]:
+            if wordsearch[i][j] == pattern[0]:
                 position = Position(i, j)
-                feasible_directions = check_feasible_directions(position=position, max_index=len(pattern)-1, grid_size=(grid_width, grid_height), directions=directions)
+                feasible_directions = check_feasible_directions(position=position, max_index=len(pattern)-1, wordsearch_size=(wordsearch_height, wordsearch_width), directions=directions)
 
                 for d in feasible_directions:
-                    match = check_for_pattern_match(grid=grid, position=position, pattern=pattern, direction=d)
+                    match = check_for_pattern_match(wordsearch=wordsearch, position=position, pattern=pattern, direction=d)
                     if match:
                         count += 1
 
     return count
  
 
-def get_cross_pattern_count_in_grid(input_path: str, pattern: str, directions: List[tuple]) -> int:
+def get_cross_pattern_count_in_wordsearch(input_path: str, pattern: str, directions: List[tuple]) -> int:
 
-    grid = create_grid(input_path=input_path)
-    grid_height = len(grid)
-    grid_width = len(grid[0])
+    wordsearch = read_wordsearch(input_path=input_path)
+    wordsearch_height = len(wordsearch)
+    wordsearch_width = len(wordsearch[0])
 
     pattern = list(pattern)
 
     count = 0
-    for j in range(grid_height):
-        for i in range(grid_width):
+    for i in range(wordsearch_height):
+        for j in range(wordsearch_width):
 
-            if grid[j][i] == pattern[len(pattern)//2]:   # if grid element == middle pattern character
+            if wordsearch[i][j] == pattern[len(pattern)//2]:   # if grid element == middle pattern character
                 position = Position(i, j)
-                feasible_directions = check_feasible_directions(position=position, max_index=1, grid_size=(grid_width, grid_height), directions=directions)
+                feasible_directions = check_feasible_directions(position=position, max_index=1, wordsearch_size=(wordsearch_height, wordsearch_width), directions=directions)
                 if feasible_directions == directions:
-                    cross_match = check_for_cross_pattern_match(grid=grid, position=position, pattern=pattern, directions=feasible_directions)
+                    cross_match = check_for_cross_pattern_match(wordsearch=wordsearch, position=position, pattern=pattern, directions=feasible_directions)
                     if cross_match:
                         count += 1
     
@@ -149,10 +162,10 @@ def get_cross_pattern_count_in_grid(input_path: str, pattern: str, directions: L
 def main(input_path: str, pattern: str, directions: List[tuple], cross_pattern: str, cross_directions: List[tuple]) -> None:
 
     # part 1
-    pattern_count = get_pattern_count_in_grid(input_path=input_path, pattern=pattern, directions=directions)
+    pattern_count = get_pattern_count_in_wordsearch(input_path=input_path, pattern=pattern, directions=directions)
     print(pattern_count)
 
-    cross_pattern_count = get_cross_pattern_count_in_grid(input_path=input_path, pattern=cross_pattern, directions=cross_directions)
+    cross_pattern_count = get_cross_pattern_count_in_wordsearch(input_path=input_path, pattern=cross_pattern, directions=cross_directions)
     print(cross_pattern_count)
 
 
--- day04/test/test_day04.py
+++ day04/test/test_day04.py
@@ -43,43 +43,43 @@ test_feasible_directions = [
 
 
 @pytest.mark.parametrize("position, size, expected", test_positions)
-def test_check_position_in_grid(position, size, expected):
-    in_bound = puzzle.check_position_in_grid(position=position, grid_size=size)
+def test_check_position_in_wordsearch(position, size, expected):
+    in_bound = puzzle.check_position_in_wordsearch(position=position, wordsearch_size=size)
 
     assert in_bound == expected
 
 
 @pytest.mark.parametrize("position, test_pattern, size, expected", test_feasible_directions)
 def test_check_feasible_directions(position, test_pattern, size, expected, directions):
-    feasible_directions = puzzle.check_feasible_directions(position=position, max_index=len(test_pattern)-1, grid_size=size, directions=directions)
+    feasible_directions = puzzle.check_feasible_directions(position=position, max_index=len(test_pattern)-1, wordsearch_size=size, directions=directions)
 
     assert all(elem in expected for elem in feasible_directions)
 
 
 def test_check_for_pattern_match(input_path, pattern):
-    grid = puzzle.create_grid(input_path=input_path)
-    position = puzzle.Position(5, 0)
-    direction = (1, 0)
+    wordsearch = puzzle.read_wordsearch(input_path=input_path)
+    position = puzzle.Position(0, 5)
+    direction = (0, 1)
 
-    match = puzzle.check_for_pattern_match(grid=grid, position=position, pattern=list(pattern), direction=direction)
+    match = puzzle.check_for_pattern_match(wordsearch=wordsearch, position=position, pattern=list(pattern), direction=direction)
     assert match == True
 
 
 def test_check_for_cross_pattern_match(input_path, cross_pattern, cross_directions):
-    grid = puzzle.create_grid(input_path=input_path)
-    position = puzzle.Position(2, 1)
+    wordsearch = puzzle.read_wordsearch(input_path=input_path)
+    position = puzzle.Position(1, 2)
 
-    match = puzzle.check_for_cross_pattern_match(grid=grid, position=position, pattern=list(cross_pattern), directions=cross_directions)
+    match = puzzle.check_for_cross_pattern_match(wordsearch=wordsearch, position=position, pattern=list(cross_pattern), directions=cross_directions)
     assert match == True
 
 
-def test_get_pattern_count_in_grid(input_path, pattern, directions):
-    pattern_count = puzzle.get_pattern_count_in_grid(input_path=input_path, pattern=pattern, directions=directions)
+def test_get_pattern_count_in_wordsearch(input_path, pattern, directions):
+    pattern_count = puzzle.get_pattern_count_in_wordsearch(input_path=input_path, pattern=pattern, directions=directions)
 
     assert pattern_count == 18
 
 
-def test_cross_pattern_count_in_grid(input_path, cross_pattern, cross_directions):
-    cross_pattern_count = puzzle.get_cross_pattern_count_in_grid(input_path=input_path, pattern=cross_pattern, directions=cross_directions)
+def test_get_cross_pattern_count_in_wordsearch(input_path, cross_pattern, cross_directions):
+    cross_pattern_count = puzzle.get_cross_pattern_count_in_wordsearch(input_path=input_path, pattern=cross_pattern, directions=cross_directions)
 
     assert cross_pattern_count == 9
